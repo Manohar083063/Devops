56857daf9118ef1ac4a33e4484998082aaf5d377
Manohar083063 <76534870+Manohar083063@users.noreply.github.com>
2021-01-23 20:53:29 -0600

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,11 +1,9 @@
-/* This is a simple Java program. 
-   FileName : "HelloWorld.java". */
-class HelloWorld 
-{ 
-    // Your program begins with a call to main(). 
-    // Prints "Hello, World" to the terminal window. 
-    public static void main(String args[]) 
-    { 
-        System.out.println("Hello, World"); 
-    } 
-} 
+import java.lang.*;
+public class Devops
+{
+  public static void main(String args[])
+  {
+    System.out.println("Good Morning");
+    System.out.println("Hello World");
+  }
+}
